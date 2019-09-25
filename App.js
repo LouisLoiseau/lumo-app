5b62b1969d69cf3c58c78b999c08590bb1a6a1c7
Zya04 <zya.nassurally@supinternet.fr>
2019-09-25 17:01:29 +0200

    init app on expo

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image  style={{width: 125, height: 125}}
+              source={require("./assets/Logo_png.png")}
+      />
+      <Text>Bienvenue sur Lumo </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
