7b24ce19d7a0cb1b583168ed8938c7d4d9b0076a
Louis Loiseau-Billon <louis.loiseau-billon@supinternet.fr>
2019-10-22 10:26:56 +0200

    Moved to typescript

--- App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import RootContainer from "./App/Screens/RootContainer";
-
-export default function App() {
-  return (
-    <RootContainer />
-  );
-}
--- App/Navigation/index.js
+++
@@ -1,4 +0,0 @@
-import { createAppContainer } from 'react-navigation';
-import MainNavigator from './MainNavigator';
-
-export default createAppContainer(MainNavigator);
