5a0d6a724991e9b662249a4aeb34af90e3dbf11f
Louis Loiseau-Billon <louis.loiseau-billon@supinternet.fr>
2019-11-07 18:55:47 +0100

    Added component to wrap code input

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,9 @@
 import Button from './Button';
 import Container from './Container';
+import CodeInput from './CodeInput';
 
 export {
   Button,
   Container,
+  CodeInput,
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,86 +1,36 @@
 import React from 'react';
-import { Text, View, TextInput, TouchableWithoutFeedback} from 'react-native';
-import { Container } from '@/components';
+import { Text, View } from 'react-native';
+import { Container, CodeInput } from '@/components';
 import styles from './styles/HomeScreenStyles';
-const CODE_LENGTH = new Array(6).fill(0);
+import { NavigationStackProp } from 'react-navigation-stack';
 
-class HomeScreen extends React.Component {
-	input = React.createRef();
-	state = {
-		value: "",
-		focused: false,
-	};
+export interface State {
+	code: string;
+}
 
-	handleClick = () => {
-		this.input.current.focus();
-	};
-	handleFocus = () => {
-		this.setState({ focused: true });
-	};
-	handleBlur = () => {
-		this.setState({
-			focused: false,
-		});
-	};
-	handleKeyPress = e => {
-		if (e.nativeEvent.key === "Backspace") {
-			this.setState(state => {
-				return {
-					value: state.value.slice(0, state.value.length - 1),
-				};
-			});
-		}
-	};
-	handleChange = value => {
-		this.setState(state => {
-			if (state.value.length >= CODE_LENGTH.length) return null;
-			return {
-				value: (state.value + value).slice(0, CODE_LENGTH.length),
-			};
-		});
-	};
+export interface Props {
+	navigation?: NavigationStackProp;
+}
+
+class HomeScreen extends React.Component<Props, State> {
+	
+	constructor(props) {
+		super(props);
+		this.state = {
+			code: ''
+		};
+	}
+
+	handleValueChange = value => {
+		this.setState({ code: value });
+	}
+
 	render() {
-		const { value, focused } = this.state;
-		const values = value.split("");
-		const selectedIndex = values.length < CODE_LENGTH.length ? values.length : CODE_LENGTH.length - 1;
-		const hideInput = !(values.length < CODE_LENGTH.length);
-
 		return (
 			<Container>
-				<Text>HomeScreen</Text>
-				
+				<Text>{this.state.code}</Text>
 				<View style={styles.container}>
-					<TouchableWithoutFeedback onPress={this.handleClick}>
-						<View style={styles.wrap}>
-							<TextInput
-								value=""
-								ref={this.input}
-								onChangeText={this.handleChange}
-								onKeyPress={this.handleKeyPress}
-								onFocus={this.handleFocus}
-								onBlur={this.handleBlur}
-								style={[
-									styles.input,
-									{
-										left: selectedIndex * 32,
-										opacity: hideInput ? 0 : 1,
-									},
-								]}
-							/>
-							{CODE_LENGTH.map((v, index) => {
-								const selected = values.length === index;
-								const filled = values.length === CODE_LENGTH.length && index === CODE_LENGTH.length - 1;
-								const removeBorder = index === CODE_LENGTH.length - 1 ? styles.noBorder : undefined;
-								
-								return (
-									<View style={[styles.display, removeBorder]} key={index}>
-										<Text style={styles.text}>{values[index] || ""}</Text>
-										{(selected || filled) && focused && <View style={styles.shadows} />}
-									</View>
-								);
-							})}
-						</View>
-					</TouchableWithoutFeedback>
+					<CodeInput codeLength={6} onValueChange={this.handleValueChange}/>
 				</View>
 			</Container>
 		);
