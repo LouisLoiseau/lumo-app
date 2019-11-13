d1d37661173f117bdeb882fe4519507e6aa0d40e
Louis Loiseau-Billon <louis.loiseau-billon@supinternet.fr>
2019-11-14 00:37:41 +0100

    Fixed questions on coursemodal went down for no reason

--- src/components/styles/CourseModalStyles.tsx
+++ src/components/styles/CourseModalStyles.tsx
@@ -22,6 +22,10 @@ export default StyleSheet.create({
     // height: Metrics.realHeight,
   },
 
+  stepImageSpeaker: {
+    position: 'absolute',
+  },
+
   stepImageBg:{
     width: '100%',
     height: '100%',
